b04e9499174414ccf56f18ca37ac3beaa972ea0a
yjun0915 <kyjun09150916@gmail.com>
2025-01-20 14:18:15 +0900

    [comment] comment about button widget

--- Figure_Construct/main.py
+++ Figure_Construct/main.py
@@ -1,20 +1,20 @@
 import matplotlib                               # 데이터베이스 변경을 위해 호출
 import matplotlib.pyplot as plt                 # 사용할 라이브러리
-from matplotlib.widgets import Button
-import numpy as np
+from matplotlib.widgets import Button           # 버튼 라이브러리
+import numpy as np                              # 임시 데이터 생성 및 수학 라이브러리
 
 matplotlib.use('TkAgg')     # 기본 백엔드인 macosx는 fig.canvas.manager.window.geometry()를 지원하지 않아 TkAgg로 변경
 
 fig01 = plt.figure(1, (12., 8.), 100)   # figure를 넘버링하여 지정(중복❎)
-fig01.suptitle("matplotlib")                            # figure 별로 제목 지정 가능
-fig01.canvas.manager.window.geometry("+100+100")        # 화면 상에서의 윈도우의 위치 지정
-fig01.subplots_adjust(bottom=0.2)
+fig01.suptitle("matplotlib")                             # figure 별로 제목 지정 가능
+fig01.canvas.manager.window.geometry("+100+100")         # 화면 상에서의 윈도우의 위치 지정
+fig01.subplots_adjust(bottom=0.2)                        # 버튼을 위한 자리 마련
 
 fig02 = plt.figure(2, (3., 8.), 100)   # figsize는 리스트([])가 아닌 튜플(())로 지정하여 warning 방지
 fig02.suptitle("Sub Fig")
 fig02.canvas.manager.window.geometry("+1300+100")        # 문자열 형식만 지원하는 듯
 
-ax1 = fig01.add_subplot(231)        # 기본적인 axes
+ax1 = fig01.add_subplot(231)            # 기본적인 axes
 ax2 = fig01.add_subplot(234)
 ax3 = fig01.add_subplot(2, 3, (2, 6))   # 여러 인접한 axes를 묶어 하나로 표현
 
